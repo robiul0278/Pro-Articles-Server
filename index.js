72aed6ef47ccbf4629e8a87c977d149c9422762f
imranahmed20 <108649863+imranahmed20@users.noreply.github.com>
2023-09-04 21:48:43 +0600

    add some

--- index.js
+++ index.js
@@ -279,6 +279,8 @@ async function run() {
       res.send(result);
     });
 
+    // Comment part
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
